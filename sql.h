aa934229a08195360453cee5688fb937631c39ad
Ondrej Jirman <megous@megous.com>
2005-06-03 11:45:01 +0000

    fixed sql example comments

--- sql.h
+++ sql.h
@@ -59,22 +59,22 @@ int main(int ac, char* av[])
 
   sql_push_context(SQL_ERRJUMP);
   if (setjmp(sql_errjmp) == 1)
-  { /* exception occured */
+  {
     sql_pop_context();
     fprintf(stderr, "%s\n", sql_error());
     sql_close();
     exit(1);
   }
 
-  /* open database */
+  // open database
   sql_open("test.db");
 
   if (!sql_table_exist("tab"))
   {
-    /* create table */
+    // create table
     sql_exec("CREATE TABLE tab(id INTEGER PRIMARY KEY, name TEXT, age INTEGER DEFAULT 0);");
 
-    /* fill the table */
+    // fill the table
     q = sql_prep("INSERT INTO tab(name) VALUES(?);");
     name = names;
     while (*name != 0)
@@ -86,17 +86,17 @@ int main(int ac, char* av[])
     }
     sql_fini(q);
   }
-  /* update table */
+  // update table
   sql_exec("UPDATE tab SET age = %d WHERE name == '%q';", 20, "bob");
   sql_exec("UPDATE tab SET age = %d WHERE name == '%q';", 33, "bill");
 
-  /* query table */
+  // query table
   q = sql_prep("SELECT id,name,age FROM tab;");
   while (sql_step(q))
     printf("%d: %s (%d)\n", sql_get_int(q,0), sql_get_text(q,1), sql_get_int(q,2));
   sql_fini(q);
 
-  /* close database */
+  // close database
   sql_pop_context();
   sql_close();
   return 0;
