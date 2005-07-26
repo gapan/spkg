bbedb58f35ff4b436ec9fd9e9669fccb96034bf0
Ondrej Jirman <megous@megous.com>
2005-07-26 21:24:08 +0000

    Updated web

--- docs/web/index.php
+++ docs/web/index.php
@@ -29,8 +29,8 @@
  <h1>News</h1>
 
   <p style="font-size:140%;font-weight:bold;color:red;padding:0;
-  border:1px solid yellow;background:white;text-align:center;">@SPKG@ 
-  @VER@ released!</p>
+  border:1px solid yellow;background:white;text-align:center;">
+  @SPKG@-@VER@ "The Speedbringer" was released!</p>
 
   <p>See <a href="/status.php">status</a> page for the current status
   and the roadmap of the @SPKG@ development.</p>
@@ -39,12 +39,17 @@
    <dt>2005-07-20</dt>
     <dd>Firtst alpha version released! This version implements install
     command with four modes of operation: brutal, normal, cautious and
-    paranoid. See documentation for more information. Other new features
+    paranoid. See manpage for more information. Other new features
     include fully functional command line interface with corresponding
     manpage and implementation of safe break points. You can safely break
     installation using some reasonable signal like SIGINT and all changes
     made so far during installation will be automatically rolled back. 
-    Enjoy it!</dd>
+    From developer's point of view: new locking code, better makefile,
+    implemented generic data query functions for both legacy and spkg
+    databases, spkg no longer depends on rm and mkdir programs, implemented
+    very robust path simplification functions, assertions usage audit,
+    better signal handling, reimplemented db sync functions, better
+    verbosity selection. Enjoy it!</dd>
 
    <dt><a href="/history.php">older news</a>...</dt>
   </dl>
