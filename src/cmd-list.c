0fda3a3b33350bdbfa1e5ba9a0a38ad24dedb294
Ondrej Jirman <megous@megous.com>
2006-07-09 14:53:37 +0200

    implement verbose package listing

--- src/cmd-list.c
+++ src/cmd-list.c
@@ -35,6 +35,14 @@ static gint _list_selector(const struct db_pkg* p, GSList* args)
   return -1;
 }
 
+static gchar* _get_date(time_t t)
+{
+  static gchar buf[100];
+  struct tm* ts = localtime(&t);
+  strftime(buf, sizeof(buf), "%F %T", ts);
+  return buf;
+}
+
 /* public 
  ************************************************************************/
 
@@ -43,16 +51,40 @@ gint cmd_list(GSList* arglist, const struct cmd_options* opts, struct error* e)
   g_assert(opts != 0);
   g_assert(e != 0);
 
-  GSList* list = db_query((db_selector)_list_selector, arglist, DB_QUERY_NAMES);
+  gint verbose = opts->verbosity > 1;
+  db_query_type type = verbose ? DB_QUERY_PKGS_WITHOUT_FILES : DB_QUERY_NAMES;
+
+  GSList* list = db_query((db_selector)_list_selector, arglist, type);
   if (!e_ok(e))
   {
     e_set(E_ERROR, "query failed");
     return 1;
   }
+
   GSList* i;
   for (i = list; i != 0; i = i->next)
-    printf("%s\n", (gchar*)i->data);
-  db_free_query(list, DB_QUERY_NAMES);
+  {
+    if (verbose)
+    {
+      struct db_pkg* pkg = i->data;
+      printf("+===================================================================+\n");
+      printf("| %-65s |\n", pkg->name);
+      printf("+===================================================================+\n");
+      printf("NAME:    %s\n", pkg->shortname);
+      printf("VERSION: %s\n", pkg->version);
+      printf("ARCH:    %s\n", pkg->arch);
+      printf("BUILD:   %s\n", pkg->build);
+      printf("DATE:    %s\n", _get_date(pkg->time));
+      printf("CSIZE:   %d kB\n", pkg->csize);
+      printf("USIZE:   %d kB\n", pkg->usize);
+      if (pkg->desc)
+        printf("%s", pkg->desc);
+    }
+    else
+      printf("%s\n", (gchar*)i->data);
+  }
+
+  db_free_query(list, type);
 
   return 0;
 }
