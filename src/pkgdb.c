1aeb0fb01d734ab4e14940b3ad6e4fdddee60d1b
Ondrej Jirman <megous@megous.com>
2006-07-09 12:04:39 +0200

    bad retval in dbopen check

--- src/pkgdb.c
+++ src/pkgdb.c
@@ -222,7 +222,7 @@ void db_filelist_add_pkg_files(const struct db_pkg* pkg)
 
 gint db_filelist_load(gboolean force_reload)
 {
-  _db_open_check(0)
+  _db_open_check(1)
 
   if (_db.filelist_loaded && !force_reload)
     return 0;
