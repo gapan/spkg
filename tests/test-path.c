8cd447f33e0ae8c2750567f455683b1a6e33c250
Ondrej Jirman <megous@megous.com>
2006-07-14 11:13:09 +0200

    update path tests

--- tests/test-path.c
+++ tests/test-path.c
@@ -15,10 +15,20 @@ static gint retval = 0;
 
 void simplify_test(gchar* in, gchar* ok)
 {
-  start_timer(0);
   gchar* out = path_simplify(in);
-  stop_timer(0);
-  print_timer(0, "time");
+  if (strcmp(out, ok))
+  {
+    printf("FAILED: \"%s\" -> \"%s\" (should be \"%s\")\n", in, out, ok);
+    retval = 1;
+  }
+  else
+    printf("OK: \"%s\" -> \"%s\"\n", in, out);
+  g_free(out);
+}
+
+void sanitize_root_test(gchar* in, gchar* ok)
+{
+  gchar* out = sanitize_root_path(in);
   if (strcmp(out, ok))
   {
     printf("FAILED: \"%s\" -> \"%s\" (should be \"%s\")\n", in, out, ok);
@@ -31,12 +41,11 @@ void simplify_test(gchar* in, gchar* ok)
 
 int main(int ac, char* av[])
 {
-  simplify_test("//a/b/.//c/.", "/a/b/c");
+  printf("simplify path tests\n");
 
-  start_timer(0);
-  path_sanitize_slashes("//a/b/.//c/.");
-  stop_timer(0);
-  print_timer(0, "sl");
+  simplify_test("//a/b/.//c/.", "/a/b/c");
+  simplify_test("./", "");
+  simplify_test("/", "/");
 
   simplify_test("/a/b/../c", "/a/c");
   simplify_test("/a/b/../../../c", "/c");
@@ -46,10 +55,20 @@ int main(int ac, char* av[])
   simplify_test("a/b/../../../c", "../c");
   simplify_test("a/b/../../c/../d", "d");
   simplify_test("a/b/../../c/../../d", "../d");
-  simplify_test("a/../b/../../c/../../d", "../../d");
+  simplify_test("a/../b/../..//c/../../d", "../../d");
   simplify_test("a/b/.././c/../../d/../../../", "../..");
-  simplify_test("a/b/.././c/../../d/../../../e/f/.././g", "../../e/g");
+  simplify_test("a/b/.././c/../../d/..//../../e//f//.././g", "../../e/g");
   simplify_test("../a/b/.././c/../../d/../../../e/f/../../.././g", "../../../../g");
+
+  printf("sanitize root path tests\n");
+
+  sanitize_root_test(NULL, "/");
+  sanitize_root_test("/", "/");
+  sanitize_root_test("//", "/");
+  sanitize_root_test("", "");
+  sanitize_root_test("./", "");
+  sanitize_root_test("./bob", "bob/");
+  sanitize_root_test("bob", "bob/");
 
   return retval;
 }
