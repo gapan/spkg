8cdd9e1c8a2f0f1b7176a80bb402887f747c0ace
Ondrej Jirman <megous@megous.com>
2007-01-27 15:42:55 +0100

    cleanup signal handling

--- src/sigtrap.c
+++ src/sigtrap.c
@@ -13,22 +13,10 @@
 /* private 
  ************************************************************************/
 
-void _sig_handler(int sig)
+static void _signal_handler(int sig)
 {
-  switch (sig)
-  {
-    case SIGINT:
-    case SIGQUIT:
-    case SIGTERM:
-    case SIGHUP:
-      sig_break = 1;
-    break;
-    case SIGUSR1:
-    case SIGUSR2:
-    case SIGPIPE:
-    default:
-    break;
-  }
+  if (sig == SIGINT || sig == SIGQUIT || sig == SIGTERM || sig == SIGHUP)
+    sig_break = 1;
 }
 
 #define e_set(n, fmt, args...) e_add(e, "sigtrap", __func__, n, fmt, ##args)
@@ -42,16 +30,17 @@ gint sig_trap(struct error* e)
 {
   g_assert(e != 0);
   struct sigaction act;
-  act.sa_handler = _sig_handler;
-  act.sa_flags = 0;
+  memset(&act, 0, sizeof(act));
+  act.sa_handler = _signal_handler;
+  act.sa_flags = SA_RESTART;
   if ( sigemptyset(&act.sa_mask) ||
-       sigaction(SIGINT, &act, 0) ||
-       sigaction(SIGQUIT, &act, 0) ||
-       sigaction(SIGHUP, &act, 0) ||
-       sigaction(SIGTERM, &act, 0) ||
-       sigaction(SIGUSR1, &act, 0) ||
-       sigaction(SIGUSR2, &act, 0) ||
-       sigaction(SIGPIPE, &act, 0) )
+       sigaction(SIGINT, &act, NULL) ||
+       sigaction(SIGQUIT, &act, NULL) ||
+       sigaction(SIGHUP, &act, NULL) ||
+       sigaction(SIGTERM, &act, NULL) ||
+       sigaction(SIGUSR1, &act, NULL) ||
+       sigaction(SIGUSR2, &act, NULL) ||
+       sigaction(SIGPIPE, &act, NULL) )
   {
     e_set(E_FATAL, "signal trapping setup failed");
     return 1;
