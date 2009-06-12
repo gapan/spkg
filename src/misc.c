513c4429dd92adaaadf5d96f5617921262e6813f
Ondrej Jirman <megous@megous.com>
2009-06-12 20:53:59 +0200

    Fix for the txz package name parsing bug

--- src/misc.c
+++ src/misc.c
@@ -35,6 +35,7 @@ gchar* parse_pkgname(const gchar* path, guint elem)
       guint suffix_len =
         g_str_has_suffix(path, ".tgz") ||
         g_str_has_suffix(path, ".tlz") ||
+        g_str_has_suffix(path, ".txz") ||
         g_str_has_suffix(path, ".tar") ? 4 : 0;
       name = g_strndup(tmp, strlen(tmp) - suffix_len);
       /* 3 dashes required */
