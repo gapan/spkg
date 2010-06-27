17dc35a6151651849f58d809cb4d348f43d889d7
Ondrej Jirman <megous@megous.com>
2010-06-27 08:38:18 +0200

    Updated website

--- docs/web/common.php
+++ docs/web/common.php
@@ -57,7 +57,7 @@ else
  </div>
 
  <div id="botmenu">
- Written by Ondøej Jirman, 2005 - 2006<br/>
+ Written by Ondøej Jirman, 2005 - 2010<br/>
  Last update: <?php echo $reldate; ?><br/>
  Contact: <a href="mailto:megous@megous.com">Ondøej Jirman</a> (<a href="http://megous.com">megous.com</a>)<br/>
  </div>
@@ -69,6 +69,21 @@ else
  </div>
 
 </div>
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-4356236-3']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
 </body>
 </html>
 <?php } head(); ?>
